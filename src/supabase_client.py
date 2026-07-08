1617b3e7898896896f8c9c100cde186fe78c3ded
proyecto colsoft <nicolasalferez20@gmail.com>
2026-07-08 14:58:14 -0500

    arreglo supabaseclient

--- src/supabase_client.py
+++ src/supabase_client.py
@@ -1,6 +1,5 @@
 import os
 from supabase import create_client, Client
-import supabase
 
 # 1. Inicializar el cliente usando las variables de entorno
 SUPABASE_URL = os.getenv("SUPABASE_URL")
@@ -33,7 +32,7 @@ def subir_pdf_supabase(ruta_pdf: str, nombre_archivo: str) -> str:
 
 def eliminar_pdf_supabase(nombre_archivo: str):
     try:
-        supabase.storage.from_(BUCKET_NAME).remove([nombre_archivo])
+        supabase_client.storage.from_(BUCKET_NAME).remove([nombre_archivo])
         return True
     except Exception as e:
         print(e)
